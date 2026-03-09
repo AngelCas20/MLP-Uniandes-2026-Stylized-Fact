e17a1286f67928a86b4e03629c62d3e19e562e47
AngelCas20 <angelcastillo2060@gmail.com>
2026-03-08 21:30:51 -0500

    update repo geometric growth and main data

--- 02_wrangle/code/01_wrangle_data.R
+++ 02_wrangle/code/01_wrangle_data.R
@@ -18,16 +18,22 @@ pwt = import('01_data/output/01_pwt110.rds',
 
 ##==: 2. Wrangle data
 
-### 2.1 Clean World Bank data
-wb = wb %>% 
-     slice(-1) %>% 
-     filter(group == 'all') %>% 
-     select(year,isocode = codewb,incomegroupwb24,share_adults_financial_account =  account_t_d) %>% 
-     mutate(share_adults_financial_account = as.numeric(share_adults_financial_account),
-            year = as.numeric(year)) %>% 
-     arrange(isocode,year) 
+### 2.1 Clean Imf data
+imf = imf %>% 
+      select(country,indicator,type_of_transformation,year = time_period,value = obs_value) %>% 
+      filter(str_detect(indicator,'Number of deposit accounts, Commercial banks')) %>% 
+      filter(str_detect(indicator,'Households')==F) 
 
-### 2.2 Clean Penn World Table
+### 2.2 Remove add isocode and remove na values
+imf = imf %>% 
+      mutate(isocode = countrycode(country,origin = 'country.name',destination = 'iso3c')) %>% 
+      drop_na(year,value,isocode) 
+
+### 2.3 Rearrange dataset
+imf = imf %>% 
+      select(year,isocode,number_accounts_1000_adults = value)
+
+### 2.4 Clean Penn World Table
 pwt = pwt %>% 
       mutate(gdp_per_capita = rgdpo/pop,
              capital_per_capita = rnna/pop) %>% 
@@ -35,42 +41,38 @@ pwt = pwt %>%
       arrange(isocode,year) %>% 
       drop_na(capital_per_capita)
 
-##==: 3. Filter for analysis period and balance panel
+##==: 3. Prepare data for for analysis 
 
-interest_period = c(2011,2021)
+### 3.1 Merge dataset
+data = inner_join(x = pwt,y = imf,by = c('year','isocode'))
 
-wb = wb %>% 
-     filter(year %in% interest_period) %>% 
-     group_by(isocode) %>% 
-     filter(n() == 2) %>% 
-     ungroup()
-
-pwt = pwt %>% 
-      filter(year %in% interest_period) %>% 
-      group_by(isocode) %>% 
-      filter(n() == 2) %>% 
-      ungroup()
-
-##==: 4. Merge dataset
-
-data = inner_join(x = wb,y = pwt,by = c('year','isocode')) %>% 
-       relocate(year,isocode,country,income_group = incomegroupwb24)
-
-### 4.1 Remove venezuela from dataset
+### 3.2 Exclude Venezuela
 data = data %>% 
        filter(isocode != 'VEN')
 
-##==: 5. Compute growth rate
+##==: 4. Compute geometric growth rate dataset
 
+### 4.1 Set time period
+interest_period = c(2004,2023)
+
+### 4.2 Compute time difference
 time_diff = diff(interest_period)
 
+### 4.3 Filter for interest period and balance panel
 geometric_growth = data %>% 
-                   group_by(isocode,income_group) %>% 
-                   summarise(across(.cols = c(share_adults_financial_account,gdp_per_capita,capital_per_capita),
+                   filter(year %in% interest_period) %>% 
+                   group_by(isocode) %>% 
+                   filter(n() == 2) %>% 
+                   ungroup()
+
+### 4.4 Compute geometric growth 
+geometric_growth = geometric_growth %>% 
+                   group_by(isocode) %>% 
+                   summarise(across(.cols = c(number_accounts_1000_adults,gdp_per_capita,capital_per_capita),
                                     .fns = function(x){
                                      x = (x/lag(x))^(1/time_diff) -1
                                      x = max(x,na.rm = T)
-                              })) %>% 
+                            })) %>% 
                    ungroup()
 
 ##==: 6. Export data
