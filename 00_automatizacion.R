834825412129d2f60edcbdb0080b8970e6c2c409
Yesenia455 <ypfontalvo14@gmail.com>
2026-03-14 22:34:36 -0500

    Automatización final

--- 00_automatizacion.R
+++ 00_automatizacion.R
@@ -7,12 +7,32 @@ source("00_programs/00_packages.R")
 
 
 #====== 01. lista de archivos de todo el repositorio: 
-files = list.files(path = "../MLP-Uniandes-2026-Stylized-Fact/",
+files_01 = list.files(path = "01_data/code/",
                    pattern = "\\.R$",
                    full.names = TRUE,
-                   recursive = TRUE) %>% 
-         .[str_detect(string = ., pattern = "00_automatizacion.R|00_packages.R|00_themes.R", negate = T)] 
+                   recursive = TRUE) 
+
+files_02 = list.files(path = "02_wrangle/code/",
+                   pattern = "\\.R$",
+                   full.names = TRUE,
+                   recursive = TRUE) 
+
+files_03 = list.files(path = "03_main_results/code/",
+                      pattern = "\\.R$",
+                      full.names = TRUE,
+                      recursive = TRUE) 
+
+files_04 = list.files(path = "04_robustness/code/",
+                      pattern = "\\.R$",
+                      full.names = TRUE,
+                      recursive = TRUE) 
+
+files_05 = list.files(path = "05_mechanisms/code/",
+                      pattern = "\\.R$",
+                      full.names = TRUE,
+                      recursive = TRUE) 
 
 #====== 02.  correr todos los script:
-walk(files, function(x){source(x); gc()},.progress = T)
+files = c(files_01, files_02, files_03, files_04, files_05)
 
+walk(files, function(x){source(x); gc()},.progress = T)
